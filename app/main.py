c6e8b8d3e4b36af0335ceeabf8088ae79e0e7a52
ulianamelnyk <melnykula23@gmail.com>
2026-05-02 10:05:07 +0200

    changes 2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 class Animal:
     alive = []
 
-    def __init__(self,
-                 name: str,
-                 hidden: bool = False) -> None:
-        self.health = 100
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+        self.health = health
         self.name = name
         self.hidden = hidden
         Animal.alive.append(self)
@@ -18,24 +21,20 @@ class Animal:
 
 
 class Herbivore(Animal):
-
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-
     def bite(self, animal: Animal) -> None:
-
         if not isinstance(animal, Herbivore):
             return
 
-        if animal._hidden:
+        if animal.hidden:
             return
 
         animal.health -= 50
 
         if animal.health <= 0:
             animal.health = 0
-
             Animal.alive.remove(animal)
